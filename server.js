e28cb2cecf10b205c0deb4759c51f8154dd02acd
Lazar Narcis <lnarcis310@gmail.com>
2022-01-18 21:15:57 +0200

    delete unnecessary get

--- server.js
+++ server.js
@@ -3,10 +3,6 @@ const app = express();
 const notificationsAPI = require("./server/notifications/notifications");
 const port = 3001;
 
-app.get("/", (req, res) => {
-    res.send("salut");
-});
-
 app.use("/api/notifications", notificationsAPI);
 
 app.listen(port, () => {
